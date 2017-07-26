1d14a555c265f717d481d78b9ae27849454fff28
Benjamin Fagin <bfagin@fabricgenomics.com>
2017-07-26 16:55:58 -0700

    lambda invocation permissions

--- aws_events/handlers/Handle_CloudWatch.py
+++ aws_events/handlers/Handle_CloudWatch.py
@@ -1,9 +1,20 @@
+import json
 from utils import publish_event
 
 
 def handler(event, context):
+    message_content = event['Records'][0]["Sns"]
+    message_data = json.loads(message_content['Message'])
+    subject = message_content.get('Subject', '')
+    
+    message = "Metric '{}' is in state '{}'".format(
+        message_data['Trigger']['MetricName'],
+        message_data['NewStateValue'],
+    )
+    
     publish_event(
         source='CloudWatch',
-        subject='SUBJECTT',
-        message='MESSAGEE',
+        subject=subject,
+        message=message,
+        data=message_data,
     )
