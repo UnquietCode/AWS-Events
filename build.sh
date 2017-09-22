3aafca4e20be2408adf3098ef7fd2e87971ce1f9
Benjamin Fagin <bfagin@fabricgenomics.com>
2017-09-22 12:35:28 -0700

    cleanup

--- build.sh
+++ build.sh
@@ -5,10 +5,17 @@ set -ex
 source environment.sh
 
 # package the lambda functions
-aws cloudformation package --template-file stack.yaml --output-template-file stack.new.yaml --s3-bucket $S3_BUCKET --s3-prefix "$S3_PREFIX"
+aws cloudformation package \
+  --template-file stack.yaml \
+  --output-template-file stack.new.yaml \
+  --s3-bucket "$S3_BUCKET" \
+  --s3-prefix "$S3_PREFIX"
 
 # deploy the stack
-aws cloudformation deploy --template-file stack.new.yaml --stack-name AWS-Events --capabilities CAPABILITY_NAMED_IAM \
+aws cloudformation deploy \
+  --template-file stack.new.yaml \
+  --stack-name AWS-Events \
+  --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     SlackURL="$SLACK_URL" \
     EmailSenderAddress="$EMAIL_SENDER" \
